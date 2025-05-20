4c8342e962242797b5938767c0db8003b0f0976c
나이스탑 <mistarcodm@gmail.com>
2025-05-21 01:08:34 +0900

    Add files via upload

---
+++ var_overwrite.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(){
+    char buf[0x10];
+    int idx = 0;
+    printf("나탑이 : 우으앍! 너무 어려워요!! 도와주세요!\n");
+    printf("input : ");
+    read(0, buf, 0x100);
+    
+    if(idx){
+        system("/bin/sh");
+    }
+    else{
+        printf("나탑이 : 아.. 이거 너무 어려워요.. 괜찮아요! 기회는 많으니까요!\n");
+    }
+    return 0;
+}
