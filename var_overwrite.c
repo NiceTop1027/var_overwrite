bc5c3df93d18a5ec680682d19f8c42bfdc8a91aa
나이스탑 <mistarcodm@gmail.com>
2025-05-21 01:18:15 +0900

    Update var_overwrite.c

--- var_overwrite.c
+++ var_overwrite.c
@@ -3,7 +3,14 @@
 #include <string.h>
 #include <unistd.h>
 
+void init() {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+}
+
 int main(){
+    init();
     char buf[0x10];
     int idx = 0;
     printf("나탑이 : 우으앍! 너무 어려워요!! 도와주세요!\n");
